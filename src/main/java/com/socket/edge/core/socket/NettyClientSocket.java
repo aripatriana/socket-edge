84a15b50214cd73870bf12285f0eeecc7df2922f
ari.patriana <ari.patriana@jalin.co.id>
2026-01-20 19:17:37 +0700

    rename method

--- src/main/java/com/socket/edge/core/socket/NettyClientSocket.java
+++ src/main/java/com/socket/edge/core/socket/NettyClientSocket.java
@@ -187,7 +187,7 @@ public class NettyClientSocket extends AbstractSocket {
         }
 
         this.channel = null;
-        channelPool.unregister(disconnected);
+        channelPool.removeChannel(disconnected);
 
         log.info("{} disconnected", getId());
 
