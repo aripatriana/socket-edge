4f2b6e905d7bbd64f0a74a8b1affa21d33350620
ari.patriana <ari.patriana@jalin.co.id>
2026-01-23 06:11:40 +0700

    add generate channel id

--- src/main/java/com/socket/edge/utils/CommonUtil.java
+++ src/main/java/com/socket/edge/utils/CommonUtil.java
@@ -3,8 +3,10 @@ package com.socket.edge.utils;
 import com.socket.edge.model.ChangeImpact;
 import com.socket.edge.model.helper.FieldDiff;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Objects;
+import java.util.zip.CRC32;
 
 public class CommonUtil {
 
@@ -26,5 +28,18 @@ public class CommonUtil {
 
     public static String clientId(String name, String host, int port) {
         return String.format("%s-client-%s-%d", name, host, port);
+    }
+
+    public static String channelId(String socketId, String endpointId) {
+        return String.format("%08d", identity(socketId, endpointId));
+    }
+
+    public static int identity(String id1, String id2) {
+        String combined = id1 + "|" + id2;
+
+        CRC32 crc = new CRC32();
+        crc.update(combined.getBytes(StandardCharsets.UTF_8));
+
+        return (int) (crc.getValue() % 100_000_000);
     }
 }
