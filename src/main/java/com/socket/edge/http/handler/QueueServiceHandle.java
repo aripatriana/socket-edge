938c25625eeef139a53ae4e66364e0f345bb7c46
ari.patriana <ari.patriana@jalin.co.id>
2026-01-23 07:48:00 +0700

    add queue service handler

---
+++ src/main/java/com/socket/edge/http/handler/QueueServiceHandle.java
@@ -0,0 +1,75 @@
+package com.socket.edge.http.handler;
+
+import com.socket.edge.core.TelemetryRegistry;
+import com.socket.edge.utils.JsonUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class QueueServiceHandle implements HttpServiceHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(QueueServiceHandle.class);
+    private TelemetryRegistry telemetryRegistry;
+
+    public QueueServiceHandle(TelemetryRegistry telemetryRegistry) {
+        this.telemetryRegistry = telemetryRegistry;
+    }
+
+    @Override
+    public String path() {
+        return "/socket/queues";
+    }
+
+    @Override
+    public FullHttpResponse handle(FullHttpRequest request, QueryStringDecoder decoder) {
+        String id = decoder.parameters()
+                .getOrDefault("id", List.of())
+                .stream()
+                .findFirst()
+                .orElse(null);
+
+        String name = decoder.parameters()
+                .getOrDefault("name", List.of())
+                .stream()
+                .findFirst()
+                .orElse(null);
+
+        Map<String, Object> result = new HashMap<>();
+        try {
+            if (id != null && !id.isEmpty()) {
+                if (id.equalsIgnoreCase("all")) {
+                    result.put("result", telemetryRegistry.getAllQueue());
+                } else {
+                    result.put("result", telemetryRegistry.getQueueById(id));
+                }
+            } else if (name != null && !name.isEmpty()) {
+                result.put("result", telemetryRegistry.getQueueByName(name));
+            } else {
+                result.put("message", "No action peformed");
+            }
+            result.put("status", "OK");
+        } catch (Exception e) {
+            log.error("Error {}", e.getCause());
+            result.put("status", "FAILED");
+            result.put("message", e.getMessage());
+        }
+        FullHttpResponse resp = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.copiedBuffer(JsonUtil.toJson(result), StandardCharsets.UTF_8)
+        );
+
+        resp.headers()
+                .set(HttpHeaderNames.CONTENT_TYPE, "text/plain")
+                .setInt(HttpHeaderNames.CONTENT_LENGTH,
+                        resp.content().readableBytes());
+
+        return resp;
+    }
+}
